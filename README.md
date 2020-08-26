2becb0a49fa27d90c77a5a6172c1aeaca12e644b
zhangzhongming <810307015@qq.com>
2020-08-26 17:04:35 +0800

    根据传入的模板名称动态生成模板文件

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# react-demo-cli
-一个自动生成各种react模板的cli工具
+### react-demo-cli
+
+- 一个自动生成各种react模板的cli工具
+
+#### 使用说明
+
+- 安装，`npm i create-react-auto -g`。
+- 使用：`create-react-auto [templateName]`。
+- 参数说明：`templateName`可选值
+  1. `base`，什么都没有添加的空项目
+  2. `route`，添加了多级路由的空项目
+  3. `redux`，添加了redux的空项目
+  4. `route-redux`，添加了多级路由和redux的空项目
--- bin/index.js
+++ bin/index.js
@@ -1,6 +1,54 @@
 #! /usr/bin/env node
 const fs = require('fs');
+const path = require('path');
 
 const list = process.argv;
 const type = list[2] || 'base';
+const p = path.resolve(__filename, `../../template/${type}`);
 
+const copy = function (src, dst) {
+  let paths = fs.readdirSync(src); //同步读取当前目录
+  paths.forEach(function (path) {
+    const _src = src + '/' + path;
+    const _dst = dst + '/' + path;
+    fs.stat(_src, function (err, stats) { //stats 该对象 包含文件属性
+      if (err) throw err;
+      if (stats.isFile()) { //如果是个文件则拷贝
+        let readable = fs.createReadStream(_src);//创建读取流
+        let writable = fs.createWriteStream(_dst);//创建写入流
+        readable.pipe(writable);
+        console.log(`${path} 写入完成`)
+      } else if (stats.isDirectory() && !path.includes('node_modules')) { //是目录则 递归
+        checkDirectory(_src, _dst, copy);
+      }
+    });
+  });
+}
+
+const checkDirectory = function (src, dst, callback) {
+  fs.access(dst, fs.constants.F_OK, (err) => {
+    if (err) {
+      fs.mkdirSync(dst);
+      callback(src, dst);
+    } else {
+      callback(src, dst);
+    }
+  });
+};
+
+fs.stat(p, (err, stats) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  if (stats.isDirectory()) {
+    checkDirectory(p, './', copy);
+  } else {
+    console.log('暂时没有该类型的模板');
+    console.log('当前支持模板如下');
+    console.log('base: 基本的空项目');
+    console.log('route: 带router的空项目');
+    console.log('redux: 带redux的空项目');
+    console.log('route-redux: 带redux和router的空项目');
+  }
+})
