5b8dedf7dfdc84652be24ab76c2b8ceb70012ed5
zhangzhongming <810307015@qq.com>
2020-08-27 09:26:13 +0800

    移除template中的npmignore

--- bin/index.js
+++ bin/index.js
@@ -8,27 +8,27 @@ const p = path.resolve(__filename, `../../template/${type}`);
 
 const copy = function (src, dst) {
   let paths = fs.readdirSync(src); //同步读取当前目录
-  paths.forEach(function (path) {
+  paths.forEach(async function (path) {
     const _src = src + '/' + path;
     const _dst = dst + '/' + path;
-    fs.stat(_src, function (err, stats) { //stats 该对象 包含文件属性
+    await fs.stat(_src, async function (err, stats) { //stats 该对象 包含文件属性
       if (err) throw err;
       if (stats.isFile()) { //如果是个文件则拷贝
         let readable = fs.createReadStream(_src);//创建读取流
         let writable = fs.createWriteStream(_dst);//创建写入流
-        readable.pipe(writable);
+        await readable.pipe(writable);
         console.log(`${path} 写入完成`)
-      } else if (stats.isDirectory() && !path.includes('node_modules')) { //是目录则 递归
-        checkDirectory(_src, _dst, copy);
+      } else if (stats.isDirectory() && path !=='node_modules') { //是目录则 递归
+        await checkDirectory(_src, _dst, copy);
       }
     });
   });
 }
 
 const checkDirectory = function (src, dst, callback) {
-  fs.access(dst, fs.constants.F_OK, (err) => {
+  fs.access(dst, fs.constants.F_OK, async (err) => {
     if (err) {
-      fs.mkdirSync(dst);
+      await fs.mkdirSync(dst);
       callback(src, dst);
     } else {
       callback(src, dst);
